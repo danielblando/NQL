8f84a788067efae20dabd864833447497a0af0fd
Gabriel Vieira <gluisvieira@gmail.com>
2013-11-02 12:52:52 -0200

    Bando de merda

---
+++ nql/nql.py
@@ -0,0 +1,1 @@
+from nql_parser import parser
---
+++ nql/nql_lexer_example.py
@@ -0,0 +1,16 @@
+from nql_lexer import lexer
+
+# Test it out
+data = '''
+3 + 4 * 10
+  + -20 *2
+'''
+
+# Give the lexer some input
+lexer.input(data)
+
+# Tokenize
+while True:
+    tok = lexer.token()
+    if not tok: break      # No more input
+    print tok
