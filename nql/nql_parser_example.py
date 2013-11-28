e3a3d3038c6972e7644e89456a9b62fc210ec836
Daniel Blando <ddeluigg@gmail.com>
2013-11-28 11:54:09 -0200

    Mudanca teste

--- nql/nql_parser_example.py
+++ nql/nql_parser_example.py
@@ -1,7 +1,7 @@
 from nql_parser import parser
 
 # Test it out
-data = ''' selecionar adf de alunos asdf '''
+data = ''' selecionar nome de alunos '''
 
 # Give the parser some input
 print parser.parse(data)
